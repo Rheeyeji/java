42d5c95c72c14e942dcd3c01727ddfdcdca91239
이예지 <leemimi0321@naver.com>
2017-01-19 11:21:09 +0900

    no message

--- GitTest/src/Main.java
+++ GitTest/src/Main.java
@@ -2,8 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		System.out.println("안녕하세요~^^");
+		System.out.println("반갑습니당~");
+		System.out.println("ㅎㅎ");
+		
 	}
 
 }
